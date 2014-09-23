70d4a15d7f7631f94e3e6ad6ee59502e0c8c6ff7
altyus <alctyus@gmail.com>
2014-09-23 15:19:49 -0400

    added project template

---
+++ MadLibsGenerator/app/src/main/java/com/flatironschool/madlibsgenerator/services/MadLibService.java
@@ -0,0 +1,47 @@
+package com.flatironschool.madlibsgenerator.services;
+
+import com.flatironschool.madlibsgenerator.Models.MadLib;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+
+/**
+ * Created by altyus on 9/19/14.
+ */
+public class MadLibService {
+
+    private static final String API_URL = "http://intense-fjord-5112.herokuapp.com/";
+
+    public interface LibsService {
+        @FormUrlEncoded
+        @POST("/api/v1/madlibs")
+        public void getStoryAsync(
+                @Field("places") List<String> places,
+                @Field("people") List<String> people,
+                @Field("verbs") List<String> verbs,
+                @Field("adjectives") List<String> adjectives
+                , Callback<String> callback);
+    }
+
+    public void loadStory(MadLib madLib, Callback<String>callback) {
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(API_URL)
+                .build();
+        LibsService service = restAdapter.create(LibsService.class);
+        service.getStoryAsync(madLib.getPlaces(),
+                madLib.getPeople(),
+                madLib.getVerbs(),
+                madLib.getAdjectives(),
+                callback);
+    }
+}
--- README.md
+++ README.md
@@ -11,12 +11,13 @@ Android-AndroidForDevelopersWorkshop
   * Create a model for a Madlib 
   * Connect to web service
   * Update the User Interface from the Activity
+  * Pass data from one activity to another
 
 ##To Code along
 
 	1. Clone Repo
 	2. Open Android Studio 
-	3. File -> Open 
+	3. Select File -> Open 
 	4. Select the MadLibsGenerator Folder 
 	5. Select Build -> Make Project 
 
